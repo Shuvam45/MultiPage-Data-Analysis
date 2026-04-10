6cbacbdc26f9810aa798d801af865d8f4d27f55d
Shuvam Chattopadhyay <shuvam.24bcb7059@vitapstudent.ac.in>
2026-04-11 02:10:42 +0530

    Refactor file upload and data processing logic

--- multipage/pages/Page_2.py
+++ multipage/pages/Page_2.py
@@ -23,116 +23,95 @@ with col0:
 
 uploaded_file= st.file_uploader("Choose a File", type=["csv"])
 
-file_path = os.path.join(os.path.dirname(__file__), "..", "..", "students_habits_performance.csv")
-df = pd.read_csv(file_path)
+if uploaded_file is None:
+    st.warning("Please Upload a File")
+    st.stop()
+
+df = pd.read_csv(uploaded_file)
+st.success(f"{uploaded_file.name} is successfully uploaded")
+st.divider()
 
 #Data Cleansing
 df.dropna(inplace=True)
-df.drop(["student_id"],axis=1, inplace=True)
-df.drop_duplicates()
+df.drop(["student_id"],axis=1, inplace=True, errors='ignore')
+df.drop_duplicates(inplace=True)
 df = df.dropna(subset=['gender', 'social_media_hours', 'internet_quality', 'attendance_percentage'])
-
 
 #dimensions
 col1, =st.columns(1)
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.success(f"{uploaded_file.name} is successfully uploaded")
+st.title("Important insights about the dataset")
+
+desc = df.describe()
+st.write(desc)
+st.divider()
+
+count_female = df[df['gender'] == 'Female'].shape[0]
+count_male = df[df['gender'] == 'Male'].shape[0]
+count_female_job = df[(df['gender'] == 'Female') & (df['part_time_job'] == 'Yes')].shape[0]
+count_male_job = df[(df['gender'] == 'Male') & (df['part_time_job'] == 'Yes')].shape[0]
+
+gradeA_female= df[(df['gender']=='Female') & (df['exam_score']>=80)].shape[0]
+gradeA_male= df[(df['gender']=='Male') & (df['exam_score']>=80)].shape[0]
+
+avg_exer_freq = df.groupby('age')['exercise_frequency'].mean().reset_index()
+
+col1, = st.columns(1)
+
+with col1:
+    def stream_data():
+        yield f"Total Female: {count_female}" 
+        yield f"Total Male: {count_male}"           
+        yield f"Total employed females: {count_female_job}"            
+        yield f"Total Employed Males: {count_male_job}"
+
+    def stream_data2():
+         yield f"Female A grade holders (>=80): {gradeA_female}"
+         yield f"Male A grade holders (>=80): {gradeA_male}"
+
+    with st.expander("Table Details"):
+        col2, col3=st.columns(2)
+        with col2:
+            for line in stream_data():
+                st.write(line)
+
+        with col3:
+             for line in stream_data2():
+                  st.write(line)
     st.divider()
 
-    st.title("Important insights about the dataset")
-
-    desc = df.describe()
-    st.write(desc)
-    st.divider()
-
-    count_female = df[df['gender'] == 'Female'].shape[0]
-    count_male = df[df['gender'] == 'Male'].shape[0]
-    count_female_job = df[(df['gender'] == 'Female') & (df['part_time_job'] == 'Yes')].shape[0]
-    count_male_job = df[(df['gender'] == 'Male') & (df['part_time_job'] == 'Yes')].shape[0]
-
-    gradeA_female= df[(df['gender']=='Female') & (df['exam_score']>=80)].shape[0]
-    gradeA_male= df[(df['gender']=='Male') & (df['exam_score']>=80)].shape[0]
-
-    avg_exer_freq = df.groupby('age')['exercise_frequency'].mean().reset_index()
-
-
-    col1, = st.columns(1)  # Define col1 here before the function
+    col1, col2, col3=st.columns(3, gap="large")
 
     with col1:
-        def stream_data():
+        options=st.radio("What Plot do you want to see?",
+                        ["Pie Chart", "Bar Plot", "TreeMap"],
+                        index=0)
 
-            yield f"Total Female: {count_female}" 
-            yield f"Total Male: {count_male}"           
-            yield f"Total employed females: {count_female_job}"            
-            yield f"Total Employed Males: {count_male_job}"
+    if options=="Pie Chart":
+        with col2:
+                fig1=px.pie(
+                    values=[count_female_job, count_male_job],
+                    names=["Female", "Male"],
+                    title="Gender Wise Employment Chart")
+                fig1.update_layout(height=400, margin=dict(t=50, b=50))
+                st.plotly_chart(fig1, use_container_width=True)
 
-        def stream_data2():
+        with col3:
+             fig2=px.pie(
+                 values=[gradeA_female, gradeA_male],
+                 names=["Female", "Male"],
+                 title="Grade A Student Gender Wise Plot"
+                 )
+             fig2.update_layout(height=400, margin=dict(t=50, b=50))
+             st.plotly_chart(fig2, use_container_width=True)
 
-             yield f"Female A grade holders (more than or equal to 80 marks): {gradeA_female}"
-             yield f"Male A grade holders (more than or equal to 80 marks): {gradeA_male}"
+    elif options=="Bar Plot":
+         fig1= px.bar(avg_exer_freq, x='age', y='exercise_frequency', color='exercise_frequency', title="Age Wise Average Exercise Frequency Plot", height=600)
+         st.plotly_chart(fig1, use_container_width=True)
 
+    else:
+         fig1=px.treemap(df, path=['age','gender', 'attendance_percentage'], title="Age vs Gender and Attendance Percentage TreeMap Plot", height=800)
 
-        with st.expander("Table Details"):
-            col2, col3=st.columns(2)
-            with col2:
-                for line in stream_data():
-                    st.write(line)
+         fig1.update_traces(root_color="lightgrey")
 
-            with col3:
-                 for line in stream_data2():
-                      st.write(line)
-        st.divider()
-
-        col1, col2, col3=st.columns(3, gap="large")
-
-        with col1:
-            options=st.radio("What Plot do you want to see?",
-                            ["Pie Chart", "Bar Plot", "TreeMap"],
-                            index=0)
-        
-
-        if options=="Pie Chart":
-            with col2:
-                    fig1=px.pie(df,
-                                values=[count_female_job, count_male_job],
-                                title="Gender Wise Employment Chart")
-                    fig1.update_layout(height=400, margin= dict(t=50, b=50))
-                    st.plotly_chart(fig1, use_container_width=True)
-
-            with col3:
-                 fig2=px.pie(df,
-                             values=[gradeA_female, gradeA_male],
-                             title="Grade A Student Gender Wise Plot"
-                             )
-                 fig2.update_layout(height=400, margin= dict(t=50, b=50))
-                 st.plotly_chart(fig2, use_container_width=True)
-
-        elif options=="Bar Plot":
-             fig1= px.bar(avg_exer_freq, x='age', y='exercise_frequency', color='exercise_frequency', title="Age Wise Average Exercise Frequency Plot", height=600)
-             st.plotly_chart(fig1, use_container_width=True)
-
-        else:
-             fig1=px.treemap(df, path=['age','gender', 'attendance_percentage'], title="Age vs Gender and Attendance Percentage TreeMap Plot", height=800)
-
-
-             fig1.update_traces(
-                root_color="lightgrey",
-            )
-
-             st.plotly_chart(fig1, use_container_width=True)
-
-
-
-
-else:
-    st.warning("Please Upload a File")
-    st.download_button(
-        "Download the required dataset",
-        mime="text/csv",
-        file_name="data2.csv",
-        icon=":material/download:",
-        data=df.to_csv().encode("utf-8"),
-        type="primary"
-    )
+         st.plotly_chart(fig1, use_container_width=True)
